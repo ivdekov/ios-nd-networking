06cd35c0f15614bb6419a45317ae772f70332160
Iavor Dekov <ivdekov@gmail.com>
2016-07-13 23:48:27 -0400

    Get JSON data and deserialize it

--- SleepingInTheLibrary/SleepingInTheLibrary/Constants.swift
+++ SleepingInTheLibrary/SleepingInTheLibrary/Constants.swift
@@ -27,7 +27,7 @@ struct Constants {
     
     // MARK: Flickr Parameter Values
     struct FlickrParameterValues {
-        static let APIKey = "YOUR API KEY HERE"
+        static let APIKey = "ffeece4057db274ab15eadeb0760e753"
         static let ResponseFormat = "json"
         static let DisableJSONCallback = "1" /* 1 means "yes" */
         static let GalleryPhotosMethod = "flickr.galleries.getPhotos"
--- SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
+++ SleepingInTheLibrary/SleepingInTheLibrary/ViewController.swift
@@ -42,6 +42,63 @@ class ViewController: UIViewController {
     
     private func getImageFromFlickr() {
         
-        // TODO: Write the network code here!
+        let methodParameters = [
+            Constants.FlickrParameterKeys.Method: Constants.FlickrParameterValues.GalleryPhotosMethod,
+            Constants.FlickrParameterKeys.APIKey: Constants.FlickrParameterValues.APIKey,
+            Constants.FlickrParameterKeys.GalleryID: Constants.FlickrParameterValues.GalleryID,
+            Constants.FlickrParameterKeys.Extras: Constants.FlickrParameterValues.MediumURL,
+            Constants.FlickrParameterKeys.Format: Constants.FlickrParameterValues.ResponseFormat,
+            Constants.FlickrParameterKeys.NoJSONCallback: Constants.FlickrParameterValues.DisableJSONCallback
+        ]
+        
+        let session = NSURLSession.sharedSession()
+        let urlString = Constants.Flickr.APIBaseURL + escapedParameters(methodParameters)
+        let url = NSURL(string: urlString)!
+        let request = NSURLRequest(URL: url)
+        
+        let task = session.dataTaskWithRequest(request) { (data, response, error) in
+            
+            if error == nil {
+                
+                let parsedResult: AnyObject!
+                do {
+                    parsedResult = try NSJSONSerialization.JSONObjectWithData(data!, options: .AllowFragments)
+                } catch {
+                    print("Could not parse the data as JSON: '\(data)'")
+                    return
+                }
+                
+                if let photosDictionary = parsedResult[Constants.FlickrResponseKeys.Photos] as? [String: AnyObject] {
+                    print(photosDictionary)
+                }
+            }
+        }
+        task.resume()
+    }
+    
+    // MARK: Helper for Escaping Parameters in URL
+    
+    private func escapedParameters(parameters: [String:AnyObject]) -> String {
+        
+        if parameters.isEmpty {
+            return ""
+        } else {
+            var keyValuePairs = [String]()
+            
+            for (key, value) in parameters {
+                
+                // make sure that it is a string value
+                let stringValue = "\(value)"
+                
+                // escape it
+                let escapedValue = stringValue.stringByAddingPercentEncodingWithAllowedCharacters(NSCharacterSet.URLQueryAllowedCharacterSet())
+                
+                // append it
+                keyValuePairs.append(key + "=" + "\(escapedValue!)")
+                
+            }
+            
+            return "?\(keyValuePairs.joinWithSeparator("&"))"
+        }
     }
 }
