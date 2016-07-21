318213c1c7dd6c996bbc3dd9c1acc9be176a9b2a
Iavor Dekov <ivdekov@gmail.com>
2016-07-21 08:43:53 -0400

    Finish TheMovieManagerV-2 app

--- TheMovieManager-v2/TheMovieManager/TMDBConvenience.swift
+++ TheMovieManager-v2/TheMovieManager/TMDBConvenience.swift
@@ -289,15 +289,27 @@ extension TMDBClient {
     func postToWatchlist(movie: TMDBMovie, watchlist: Bool, completionHandlerForWatchlist: (result: Int?, error: NSError?) -> Void) {
         
         /* 1. Specify parameters, the API method, and the HTTP body (if POST) */
+		let parameters = [
+			ParameterKeys.SessionID: TMDBClient.sharedInstance().sessionID!
+		]
+		
+		var mutableMethod: String = Methods.AccountIDWatchlist
+		mutableMethod = subtituteKeyInMethod(mutableMethod, key: URLKeys.UserID, value: String(TMDBClient.sharedInstance().userID!))!
+		let jsonBody = "{\"\(TMDBClient.JSONBodyKeys.MediaType)\": \"movie\",\"\(TMDBClient.JSONBodyKeys.MediaID)\": \"\(movie.id)\",\"\(TMDBClient.JSONBodyKeys.Watchlist)\": \(watchlist)}"
+		
         /* 2. Make the request */
-        /* 3. Send the desired value(s) to completion handler */
-        
-        /*
-        
-        taskForPOSTMethod(method, parameters: parameters, jsonBody: jsonBody) { (result, error) in
-        
-        }
-        
-        */
+		taskForPOSTMethod(mutableMethod, parameters: parameters, jsonBody: jsonBody) { (result, error) in
+			
+			/* 3. Send the desired value(s) to completion handler */
+			if let error = error {
+				completionHandlerForWatchlist(result: nil, error: error)
+			} else {
+				if let results = result[JSONResponseKeys.StatusCode] as? Int {
+					completionHandlerForWatchlist(result: results, error: nil)
+				} else {
+					completionHandlerForWatchlist(result: nil, error: NSError(domain: "postToWatchList parsing", code: 0, userInfo: [NSLocalizedDescriptionKey: "Could not parse postToWatchList"]))
+				}
+			}
+		}
     }
 }
